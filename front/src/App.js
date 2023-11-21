3add759b3b9b86514e3a540adee528a164af4f4e
MaximeSaliou72 <maxmimesaliou72@gmail.com>
2023-11-21 14:40:30 +0100

    Home page

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import "./assets/css/globals.scss";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <title> Tetris </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Tetris online" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+        />
       </header>
+      <BrowserRouter>
+        <header className="header">
+          <h1>Tetris</h1>
+          <div className="headerComputer">
+            <ul className="menu">
+              <li><NavLink to={`/account`}>Account</NavLink></li>
+              <li><NavLink to={`/logout`}>Logout</NavLink></li>
+            </ul>
+          </div>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<main className="notFound"><h1>404 NOT FOUND</h1></main>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
