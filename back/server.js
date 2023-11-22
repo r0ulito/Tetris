9e214bc02d09d044cdf2b2e9f527812fac413ad2
ArthurDEV44 <arthurjean-dev@outlook.fr>
2023-11-22 16:16:34 +0100

    Modification de CommonJS vers ES6 et patch probleme serveur

--- back/server.js
+++ back/server.js
@@ -1,25 +1,38 @@
-const Koa = require('koa');
-const http = require('http');
-const socketIo = require('socket.io');
-const bodyParser = require('koa-bodyparser');
-const koaJwt = require('koa-jwt');
-const config = require('./back/config');
-const authRoutes = require('./back/auth');
-const generalRoutes = require('./routes');
-const setupWebSocket = require('./websocket');
+import Koa from "koa";
+import http from "http";
+import bodyParser from "koa-bodyparser";
+import koaJwt from "koa-jwt";
+import fs from "fs";
+import authRoutes from "./auth.js";
+import generalRoutes from "./routes.js";
+import setupWebSocket from "./websocket.js";
+
+// Chargement de la configuration à partir de config.json
+const rawConfig = fs.readFileSync(new URL("./config.json", import.meta.url));
+const config = JSON.parse(rawConfig);
 
 const app = new Koa();
 const server = http.createServer(app.callback());
-const io = socketIo(server);
 
-app.use(bodyParser());
-app.use(authRoutes.routes());
-app.use(generalRoutes.routes());
-app.use(koaJwt({ secret: config.jwtSecret }).unless({ path: [/^\/public/, /^\/login/, /^\/signup/] }));
+// Importation dynamique de socket.io
+import("socket.io").then((socketIoModule) => {
+  // Création de l'instance socket.io en utilisant .attach()
+  const io = new socketIoModule.Server();
+  io.attach(server);
 
-setupWebSocket(io);
+  app.use(bodyParser());
+  app.use(authRoutes.routes());
+  app.use(generalRoutes.routes());
+  app.use(
+    koaJwt({ secret: config.jwt_secret }).unless({
+      path: [/^\/public/, /^\/login/, /^\/signup/],
+    }),
+  );
 
-const PORT = process.env.PORT || 8180;
-server.listen(PORT, () => {
+  setupWebSocket(io);
+
+  const PORT = 8180;
+  server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
