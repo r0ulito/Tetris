c886450adf226ac6929716eef854674805eedd57
MaximeSaliou72 <maxmimesaliou72@gmail.com>
2023-11-24 10:03:25 +0100

    require auth tetris page

--- front/src/views/TetrisView.js
+++ front/src/views/TetrisView.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Tetris from '../Tetris_components/Tetris';
+import RequireAuth from "../lib/auth";
 
 function TetrisView() {
+  RequireAuth()
+
   return (
     <div className='App'>
       <Tetris />
