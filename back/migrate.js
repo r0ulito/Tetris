6a8e799f3d2c478cf4594e71c276341e6ca51221
r0ulito <roulit77@gmail.com>
2023-12-19 14:12:06 +0100

    Added .env in gitignore, updated migrate.js

--- back/migrate.js
+++ back/migrate.js
@@ -1,16 +1,35 @@
 import pg from "pg";
-import fs from "fs";
-// Chargement de la configuration de la base de données
-const rawdata = fs.readFileSync(new URL("./config.json", import.meta.url));
-const dbConfig = JSON.parse(rawdata);
+
+import "dotenv/config";
+
 const { Pool } = pg;
+
+const { DATABASE, PASSWORD, PORT, USERNAME } = process.env;
+const dbConfig = {
+  database: DATABASE,
+  password: PASSWORD,
+  port: PORT,
+  user: USERNAME,
+};
+
+// Chargement de la configuration de la base de données
+
 const pool = new Pool(dbConfig);
+const createDatabase = async () => {
+  try {
+    await pool.query(`CREATE DATABASE ${DATABASE}`);
+    console.log("database created");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const createTables = async () => {
   await pool.query(
     "CREATE TABLE IF NOT EXISTS users (user_id BIGSERIAL PRIMARY KEY, user_name VARCHAR(255) NOT NULL, hashed_password VARCHAR(255) NOT NULL, user_email VARCHAR(255) NOT NULL)",
     [],
   );
+  console.log("table created");
 };
-
+createDatabase();
 createTables();
